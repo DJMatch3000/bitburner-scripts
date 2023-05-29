d2ba326dbd6a28de0be89d412f20b5ddb4677a48
David Matchefts <djmatch3000@gmail.com>
2023-05-28 22:27:22 -0400

    Added server connector (requires Singularity)

---
+++ src/connector.ts
@@ -0,0 +1,39 @@
+import { NS } from "@ns";
+
+export async function main(ns: NS): Promise<void> {
+    if (ns.args.length < 1) {
+        ns.tprint("Target not provided")
+        return
+    }
+    let target = ns.args[0].toString()
+    let path = findPath(ns, "home", target, [], ["home"])
+    if (path === undefined) {
+        ns.tprint("Path not found")
+        return
+    }
+
+    for (let s of path) {
+        ns.singularity.connect(s)
+    }
+
+    /**
+     * home -> target
+     * scan home.
+     * if target
+     */
+}
+
+function findPath(ns: NS, curServer: string, target: string, path: string[], scanned: string[]) {
+    let servs = ns.scan(curServer)
+    for (let s of servs) {
+        if (s === target) {
+            path.push(s)
+            return path
+        } 
+        else if (!scanned.includes(s)){
+            scanned.push(s)
+            return findPath(ns, s, target, path.concat(s), scanned)
+        }
+    }
+    return
+}
